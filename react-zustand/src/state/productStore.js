0ac3379e7d6e99191d269bbdabe241c972bd817f
Jimmy Vasquez <cjvasquez_98@hotmail.com>
2023-12-04 13:23:14 -0600

    initial config for zustand

---
+++ react-zustand/src/state/productStore.js
@@ -0,0 +1,17 @@
+import { devOnlyDevtools as devtools } from "./devTools";
+import { immer } from "zustand/middleware/immer";
+import create from "zustand";
+
+export const productsStore = create(
+  devtools(
+    immer((set) => ({
+      products: [],
+      getProducts: () => {
+        fetch('https://fakestoreapi.com/products')
+        .then((response) => response.json())
+        .then((data) => set({products: data}, false, {type: 'getProducts'}))
+      }
+    })),
+    { name: "React + Zustand", store: "products" }
+  )
+);
